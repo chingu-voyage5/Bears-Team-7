9a1319532d3282857cb70f419ffe6b6ae1e29130
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-14 18:47:06 -0400

    Set up middleware endpoint to use GraphQL

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 const PORT = 4000;
 
+//Set up middleware endpoint to understand GraphQL
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true //For development 
+}))
+
+//Set the connection with server
 app.listen(PORT, () => {
   console.log(`Now listenning for requests on port ${PORT}`);
 });
