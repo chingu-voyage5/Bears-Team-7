441c42a1246869b62838236259d44ce3d212e266
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-06-15 16:19:55 -0400

    Modify PlaceType and RootType schemas

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,39 +16,65 @@ const places = [{
   id: "1",
   yelp_id: "SGRmnarrNuVEsAjYdEoA0w",
   name: "El Farolito",
+  addressId: '3',
   is_closed: false,
-  lat: 37.75265,
-  lng: -122.41812,
   phone: "(415) 824-7877",
-  address: "2779 Mission St",
-  city: "San Francisco",
-  state: "CA",
-  country: "US"
+  coordsId: '1'
 }, {
   id: "2",
   yelp_id: "JARsJVKLPgs_yC3cwDnp7g",
+  addressId: '1',
   name: "La Taqueria",
   is_closed: false,
-  lat: 37.750883,
-  lng: -122.418123,
   phone: "(415) 524-7677",
-  address1: "2889 Mission St",
-  city: "San Francisco",
-  state: "CA",
-  country: "US"
+  coordsId: '2'
 }, {
   id: "3",
   yelp_id: "g0VCHer2uE5NLOEdblZuSw",
+  addressId: '2',
   name: "Taqueria Cancún",
   is_closed: false,
-  lat: 37.76047,
-  lng: -122.41951,
   phone: "(415) 224-4873",
-  address1: "2288 Mission St",
-  city: "San Francisco",
-  state: "CA",
-  country: "US"
-}]
+  coordsId: '3'
+}];
+
+const addresses = [{
+    id: '1',
+    address: "2779 Mission St",
+    city: "San Francisco",
+    state: "CA",
+    country: "US"
+  },
+  {
+    id: '2',
+    address: "2889 Sleet St",
+    city: "San Francisco",
+    state: "CA",
+    country: "US"
+  }, {
+    id: '3',
+    address: "2288 Varrick St",
+    city: "San Francisco",
+    state: "CA",
+    country: "US"
+  }
+];
+
+const coords = [{
+    id: '1',
+    lat: 37.75265,
+    lng: -122.41812
+  },
+  {
+    id: '2',
+    lat: 37.76047,
+    lng: -122.41951
+  }, {
+    id: '3',
+    lat: 37.750883,
+    lng: -122.418123
+  }
+]
 
 /**
  * TODO
@@ -58,49 +84,98 @@ const PlaceType = new GraphQLObjectType({
   name: 'Place',
   fields: () => ({
     id: {
-      type: GraphQLString
-    },
-    image: {
-      type: GraphQLString
-    },
-    name: {
-      type: GraphQLString
-    },
-    price: {
-      type: GraphQLInt
-    },
-    address: {
-      type: GraphQLString
-    },
-    city: {
-      type: GraphQLString
-    },
-    state: {
-      type: GraphQLString
-    },
-    country: {
-      type: GraphQLString
-    },
-    lat: {
-      type: GraphQLFloat
-    },
-    lng: {
-      type: GraphQLFloat
-    },
-    phone: {
-      type: GraphQLString
-    },
-    is_closed: {
-      type: GraphQLBoolean
+      type: GraphQLString,
+      description: 'Unique place id'
     },
     yelp_id: {
-      type: GraphQLString
+      type: GraphQLString,
+      description: 'Unique Yelp id'
+    },
+    image: {
+      type: GraphQLString,
+      description: 'Place photo'
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Name of the place'
+    },
+    price: {
+      type: GraphQLString,
+      description: 'Price level'
+    },
+    phone: {
+      type: GraphQLString,
+      description: 'Phone number of the place'
+    },
+    is_closed: {
+      type: GraphQLBoolean,
+      description: 'Whether place has been (permanently) closed'
     },
     counter: {
-      type: GraphQLInt
+      type: GraphQLInt,
+      description: 'Number of people recommended'
+    },
+    address_details: {
+      type: AddressType,
+      description: `Place's location, including address, city, state and country`,
+      resolve(parent) {
+        return addresses.find(address => address.id === parent.addressId);
+      }
+    },
+    coordinates: {
+      type: CoordinatesType,
+      description: `The coordinates of the place`,
+      resolve(parent) {
+        return coords.find(coord => coord.id === parent.coordsId);
+      }
     }
   })
 });
+
+const AddressType = new GraphQLObjectType({
+  name: 'AddressType',
+  description: `Place's location, including address, city, state and country`,
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      description: 'Unique id'
+    },
+    address: {
+      type: GraphQLString,
+      description: 'Street address'
+    },
+    city: {
+      type: GraphQLString,
+      description: 'City of the place'
+    },
+    state: {
+      type: GraphQLString,
+      description: 'State code of the place'
+    },
+    country: {
+      type: GraphQLString,
+      description: 'Country code of the plcae'
+    }
+  })
+});
+
+const CoordinatesType = new GraphQLObjectType({
+  name: 'CoordinatesType',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      description: 'Unique id'
+    },
+    lat: {
+      type: GraphQLFloat,
+      description: 'Latitude of the place'
+    },
+    lng: {
+      type: GraphQLFloat,
+      description: 'Longitude of the place'
+    }
+  })
+})
 
 /**
  * TODO
@@ -127,6 +202,30 @@ const RootQuery = new GraphQLObjectType({
       description: 'Returns a list of places',
       resolve(parent, args) {
         return places;
+      }
+    },
+    address: {
+      type: AddressType,
+      description: 'Returns address from a single place',
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(parent) {
+        return addresses.find(address => address.id === args.id);
+      }
+    },
+    coordinates: {
+      type: CoordinatesType,
+      description: 'Retirns coordinates from a single place',
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(parent) {
+        return coords.find(coord => coord.id === args.id);
       }
     }
   }
