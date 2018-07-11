7b5cf52afa8b1d28e7117dc2a0867cedf2ef277f
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-07-11 10:56:25 -0400

    Add schema stitching

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,35 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const yelpFusion = require('yelp-fusion');
-const qs = require('querystringify');
-const app = express();
-const PORT = 4000;
+const YelpBinding = require('./YelpBinding')
+const {
+  Prisma
+} = require('prisma-binding')
+const {
+  GraphQLServer
+} = require('graphql-yoga')
+const {
+  getUserId
+} = require('./utils')
+const resolvers = require('./resolvers')
 
-//Use for manage env variables
-require('dotenv').config();
-
-//Set up middleware endpoint to understand GraphQL
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true //For development
-}))
-
-const client = yelpFusion.client(process.env.ACCESS_TOKEN);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/:query', (req, res) => {
-  const query = qs.parse(req.params.query);
-
-  //console.log(...query);
-  client.search({
-      ...query
-    })
-    .then(response => {
-      const businesses = JSON.parse(response.body)
-      res.send(businesses)
-    })
-    .catch(e => console.log(e));
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: './src/generated/prisma.graphql',
+      endpoint: process.env.PRISMA_ENDPOINT,
+    }),
+    ylp: new YelpBinding(),
+  }),
 })
 
-//Set the connection with server
-app.listen(PORT, () => {
-  console.log(`Now listenning for requests on port ${PORT}`);
-});
+const options = {
+  port: process.env.PORT || 8080
+}
+
+server.start(options, ({
+  port
+}) => console.log(`Server is running on http://localhost:${port}`))
