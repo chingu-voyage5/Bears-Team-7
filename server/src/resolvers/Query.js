7e89216af0bbfbe977c399f7e66fb46a0dc6c58b
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-07-13 15:10:33 -0400

    Separate Yelp queries from Resto queries

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -11,26 +11,15 @@ const Query = {
       }
     }, info)
   },
-  search(parent, {
-    term,
-    location,
-    limit
-  }, context, info) {
-    return context.ylp.query.search({
-        term,
-        location,
-        limit
-      },
-      `{total business {id name price photos location {address1 city state country} coordinates{latitude longitude} categories{title}}}
-    `)
-  },
   business(parent, {
     id
   }, context, info) {
-    return context.ylp.query.business({
-        id
+    return context.db.query.business({
+        where: {
+          id
+        }
       },
-      `{id name price photos location{address1 city state country} coordinates{latitude longitude} categories{title}}`)
+      info)
   }
 }
 
